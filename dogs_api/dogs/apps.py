661a37dc5bec278dc0d5d171d4220a0bd3cef30b
sergey-royt <goodstop687@gmail.com>
2024-11-12 12:21:27 +0300

    add mypy ruff and pre-commit to dev dependencies

--- dogs_api/dogs/apps.py
+++ dogs_api/dogs/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dogs'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dogs_api.dogs"
--- dogs_api/dogs/models.py
+++ dogs_api/dogs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    name = models.CharField()
