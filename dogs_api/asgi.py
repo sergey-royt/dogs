52d3c9fbe13dc083cf93fc08f83e699440b4cbd0
sergey-royt <goodstop687@gmail.com>
2024-11-12 12:02:12 +0300

    add dogs application

--- dogs_api/asgi.py
+++ dogs_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dogs project.
+ASGI config for dogs_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dogs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dogs_api.settings')
 
 application = get_asgi_application()
