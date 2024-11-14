1440e04b49736397e16ddfe807db2d3a3568d7bd
sergey-royt <goodstop687@gmail.com>
2024-11-14 10:35:14 +0300

    refactor serializers

--- dogs_api/dogs/serializers.py
+++ dogs_api/dogs/serializers.py
@@ -1,15 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer
 
 from dogs_api.dogs.models import Dog, Breed
 
 
-class DogSerializer(ModelSerializer):
+class DogSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Dog
         fields = "__all__"
 
 
-class BreedSerializer(ModelSerializer):
+class BreedSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Breed
         fields = "__all__"
